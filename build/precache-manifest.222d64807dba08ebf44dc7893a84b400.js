d1b582e54f4ace36e1b65a1583f628bdba537e8f
Ovidiu-SBP <ovidiu@sbp-romania.com>
2020-04-21 17:52:18 +0300

    mesaj

---
+++ build/precache-manifest.222d64807dba08ebf44dc7893a84b400.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd402ea9e6eed66a1df41845f19e9e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da89c88320a4ab16b00",
+    "url": "/static/css/main.542b0183.chunk.css"
+  },
+  {
+    "revision": "4e94cb892d936457a38b",
+    "url": "/static/js/2.a4d7bb4a.chunk.js"
+  },
+  {
+    "revision": "bc399dc08cbcd7513fcbdb5e5a127199",
+    "url": "/static/js/2.a4d7bb4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da89c88320a4ab16b00",
+    "url": "/static/js/main.b0fd1936.chunk.js"
+  },
+  {
+    "revision": "2db506ab639224fcc03c",
+    "url": "/static/js/runtime-main.7921dc2b.js"
+  },
+  {
+    "revision": "3098f2c47c1daf29152c5eb8db7ee9e3",
+    "url": "/static/media/404.3098f2c4.svg"
+  },
+  {
+    "revision": "2e751adef1c245f515518b5ccba17fcf",
+    "url": "/static/media/ClassdefinitionOverview.2e751ade.md"
+  },
+  {
+    "revision": "816201fcdd9de84466d7dae531b23d72",
+    "url": "/static/media/context.816201fc.md"
+  },
+  {
+    "revision": "291acff9de4e19adcea54ad719a32c92",
+    "url": "/static/media/logo.291acff9.svg"
+  }
+]);
